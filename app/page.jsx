162e2cdaa1dfdeb1d6c2e5982a79e6a3e51778c1
owusuvictorjnr <owusuvictor80@gmail.com>
2023-08-27 12:28:26 -0400

    add some text in the main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,48 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="">
-      <main>
-        <h2>dashboard</h2>
-      </main>
+      <h2>Dashboard</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+        minus dolores veniam quae, in autem voluptatum eos impedit consectetur
+        error, ipsam quas assumenda, facilis reprehenderit suscipit vero nobis
+        odit doloremque atque! Minima amet eligendi aperiam veritatis ut.
+        Corporis, at perspiciatis molestias illum veniam voluptatem molestiae
+        quod, aliquid quas incidunt necessitatibus!
+      </p>
+
+      <div className="flex justify-center my-8">
+        <Link href="tickets">
+          <button className="btn-primary">View Tickets</button>
+        </Link>
+      </div>
+
+      <h2>Company Update</h2>
+
+      <div className="card">
+        <h3>New member of the web dev team </h3>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia sit
+          eligendi, blanditiis doloribus excepturi, nulla eos at sapiente quae
+          reprehenderit, cupiditate laboriosam necessitatibus quisquam
+          consequuntur ducimus distinctio similique fuga iste.
+        </p>
+      </div>
+
+      <div className="card">
+        <h3>New website live</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolor
+          unde sunt earum possimus qui dicta minus totam quae hic? Sequi in
+          beatae aut magni quam eos molestias deserunt? Laudantium!
+        </p>
+      </div>
     </main>
   )
 }
