7eeb4f21540e0e143ce18f02aca21d8c92b39227
owusuvictorjnr <owusuvictor80@gmail.com>
2023-08-27 12:29:21 -0400

    insert logo image

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,22 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Logo from './logo.png'
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="logo"
+        height=""
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>help desk</h1>
 
-      <Link href="/">dashboard</Link>
+      <Link href="/">Dashboard</Link>
 
       <Link href="/tickets">Tickets</Link>
     </nav>
